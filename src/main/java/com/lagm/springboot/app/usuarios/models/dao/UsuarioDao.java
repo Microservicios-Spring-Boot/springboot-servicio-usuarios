21cb88767fed5f408249565b3b4af50cab80fd29
Luis Guisado <lguisadom@gmail.com>
2022-01-13 02:24:45 -0500

    Implementando un repositorio REST para el CRUD

--- src/main/java/com/lagm/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/lagm/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -2,9 +2,13 @@ package com.lagm.springboot.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.lagm.springboot.app.usuarios.models.entity.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
+/* Expone un api rest para realizar el crud. Con path indicamos el enpoint donde se va a exportar todos los métodos del Crud. Listar, listar por id, crear, modificar, eliminar, y
+ * los métodos personalizados que tengamos */
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 	// PagingAndSortingRepository hereda de CrudRepository
 	// Trea funcionalidades extra para Ordenar y Paginar
