25b3dd8a7426fddabe52324b8e19933f16ca595d
Luis Guisado <lguisadom@gmail.com>
2022-01-15 01:51:41 -0500

    Configurando y exponer ID en la respuesta JSON del Rest RepositoryRest

---
+++ src/main/java/com/lagm/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package com.lagm.springboot.app.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.lagm.springboot.app.usuarios.models.entity.Role;
+import com.lagm.springboot.app.usuarios.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+	// Implementamos RepositoryRestConfigurer para configuraciones relacionadas al repositorio Rest
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Usuario.class, Role.class); // Mostrar IDs en el JSON
+	}
+}
